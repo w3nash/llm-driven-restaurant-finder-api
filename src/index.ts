5495de31aa702b56e119a88ef8c88425e175a4f9
Jonash Marcelino <thewwwnash@gmail.com>
2025-11-15 10:11:05 +0800

    feat: add express server with dotenv support
    - Add express and dotenv dependencies
    - Set up basic express server with environment variable support
    - Update .gitignore to exclude .env files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+    import express, { type Express, type Request, type Response } from 'express';
+    import dotenv from 'dotenv';
+
+    dotenv.config();
+
+    const app: Express = express();
+    const PORT = process.env['PORT'] || 3000;
+
+    app.get('/', (req: Request, res: Response) => {
+        res.send('Hello world!');
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
